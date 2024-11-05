a03a197453a3a8093ce80ef8afc45d7add3f318b
dhapper <dhaarshan.p@gmail.com>
2024-11-04 20:35:15 -0500

    Removed unecessary code

--- user-authenticator/src/main/java/com/authenticator/user_authenticator/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.authenticator.user_authenticator;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
